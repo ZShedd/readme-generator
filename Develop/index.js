64dcb6d3c67342d728d8d640c728284945ee53f0
Clown Supreme <zachary.h.shedd@gmail.com>
2023-02-15 19:29:06 -0800

    added questions for readme file generation in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,51 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { //title
+        type: 'input',
+        message: 'What will the title of your project be?',
+        name: 'title',
+    },
+    { //decription
+        type: 'input',
+        message: 'What are you trying to acomplish? How will this be done?',
+        name: 'decription',
+    },
+    { //installation
+        type: 'input',
+        message: 'How do you install your project for use?',
+        name: 'installation',
+    },
+    { //usage
+        type: 'input',
+        message: 'How would you tell someone how to use this project?',
+        name: 'usage',
+    },
+    { //contributors
+        ttype: 'input',
+        message: 'Who is able to work on this project?',
+        name: 'contributing',
+    },
+    { //link to github repo
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'gitHub',
+    },
+    { //email 
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    { //license
+        type: 'checkbox',
+        message: 'What is your license?',
+        name: 'license',
+        choices: ["MIT", "Apache", "BSD", "None"],
+    },
+
+
+];
 
 // TODO: Create a function to write README file
 function generateReadMe(answers) {
